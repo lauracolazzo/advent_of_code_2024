deccc3a3b5e9d6795c15024f83cafb418ba48d0b
Laura <laura.colazzo@mail.polimi.it>
2024-12-02 11:43:12 +0100

    Updated day_2

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -32,20 +32,20 @@ def is_line_safe(line_elements):
             break
     return is_line_safe
 
-safe_levels_count = 0
+safe_lines_count = 0
 with open('input_2.txt', 'r') as file:
     # Iterate over the rows of the matrix
     for line in file:
         line_elements = line.split(' ')
         # Check if the line is safe according to the rules
         if is_line_safe(line_elements):
-            safe_levels_count += 1
+            safe_lines_count += 1
 
-print(safe_levels_count) # 279
+print(safe_lines_count) # 279
 # --- End Part 1 ----------------------------------------------------------------------------- 
 
 # --- Part 2 -------------------------------------------------------------------------------- 
-safe_levels_count = 0
+safe_lines_count = 0
 
 def is_line_safe_with_one_error_tolerance(line_elements):
     i = 0
@@ -65,10 +65,7 @@ def is_line_safe_with_one_error_tolerance(line_elements):
         elif is_ascending_sequence(curr_level,next_level) != is_order_ascending:
             # Check if the line can still be considered safe by introducing 1 error tolerance
             line_elements.remove(line_elements[i+1])
-            if is_line_safe(line_elements[i:]):
-                is_line_valid = True
-            else:
-                is_line_valid = False
+            is_line_valid = is_line_safe(line_elements[i:])
             break
         # Compute the delta between 2 adjacent elements
         delta = abs(curr_level - next_level)
@@ -78,10 +75,7 @@ def is_line_safe_with_one_error_tolerance(line_elements):
         else:
             # Check if the delta can still be considered safe by introducing 1 error tolerance
             line_elements.remove(line_elements[i+1])
-            if is_line_safe(line_elements[i:]):
-                is_line_valid = True
-            else:
-                is_line_valid = False
+            is_line_valid = is_line_safe(line_elements[i:])
             break
     return is_line_valid
 
@@ -91,7 +85,7 @@ with open('input_2.txt', 'r') as file:
         line_elements = line.split(' ')
         # Check if the line is safe according to the rules
         if is_line_safe_with_one_error_tolerance(line_elements):
-            safe_levels_count += 1
+            safe_lines_count += 1
 
-print(safe_levels_count) # 343
+print(safe_lines_count) # 343
 # --- End Part 2 ----------------------------------------------------------------------------- 
